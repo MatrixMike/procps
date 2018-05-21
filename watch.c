b72f25f331af18e959fcb1ceb16fd4dbe4f61b4d
Mike J H <matrixmike@icloud.com>
2018-05-21 16:41:49 +1000

    still no watch

--- watch.c
+++ watch.c
@@ -26,7 +26,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
-#define WITH_WATCH8BIT 1  // to try and control the compilation to work
+// #define WITH_WATCH8BIT 1  // to try and control the compilation to work
 
 #include "c.h"
 #include "config.h"
